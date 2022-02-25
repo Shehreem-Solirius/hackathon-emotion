02d9b73a8dc40e56d5978d660b4aa637abca90e5
shehreem <shehreem.haddad@solirius.com>
2022-02-25 10:58:47 +0000

    Initial commit

---
+++ videotester.py
@@ -0,0 +1,55 @@
+import os
+import cv2
+import numpy as np
+from keras.preprocessing import image
+import warnings
+warnings.filterwarnings("ignore")
+from keras.preprocessing.image import load_img, img_to_array 
+from keras.models import  load_model
+import matplotlib.pyplot as plt
+import numpy as np
+
+# load model
+model = load_model("best_model.h5")
+
+
+face_haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, test_img = cap.read()  # captures frame and returns boolean value and captured image
+    if not ret:
+        continue
+    gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
+
+    faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+    cascade_classifier = cv2.CascadeClassifier('haarcascades/haarcascade_hand.xml')
+
+    for (x, y, w, h) in faces_detected:
+        while True:
+            ret, frame = cap.read()
+            frame = cv2.cvtColor(frame, 0)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            detections = cascade_classifier.detectMultiScale(gray, 1.3, 5)
+
+        predictions = model.predict(detections)
+
+        # find max indexed array
+        max_index = np.argmax(predictions[0])
+
+        emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+        predicted_emotion = emotions[max_index]
+
+        cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    resized_img = cv2.resize(test_img, (1000, 700))
+    cv2.imshow('Facial emotion analysis ', resized_img)
+
+    if cv2.waitKey(10) == ord('q'):  # wait until 'q' key is pressed
+        break
+
+cap.release()
+cv2.destroyAllWindows
+
